48e810d938e9925c8b41d0cdfcd6617a08402e9e
abhi <abhinandhsuby@proton.me>
2026-04-24 12:35:03 +0530

    add typst-snippets conditional paths

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -1,9 +1,5 @@
 local version = "v1.6.0"
 
--- Get the info for the specific plugin
-
--- local h = '~/.local/share/nvim/site/pack/core/opt/typst-snippets'
-
 vim.pack.add({
   {
     src = 'https://github.com/saghen/blink.cmp',
@@ -11,16 +7,17 @@ vim.pack.add({
   },
   -- dependencies
   'https://github.com/rafamadriz/friendly-snippets',
-  { src = '/home/abhi/projects/typst-snippets' },
-  -- { src = 'https://github.com/L3MON4D3/LuaSnip', version = 'v2.5.0' },
+  'https://github.com/abhinandh-s/typst-snippets',
   'https://github.com/neovim/nvim-lspconfig',
 })
 
--- require("luasnip.loaders.from_vscode").load {
---   exclude = { "cobol" },
--- }
-local typst_info = vim.pack.get({'typst-snippets'})[1]
-local typst_path = typst_info and typst_info.path or ""
+local typst_info = vim.pack.get({ 'typst-snippets' })[1]
+local upstream_path = typst_info and typst_info.path or ""
+local dev_path = '/home/abhi/projects/typst-snippets'
+local global = require('global')
+
+local path_to_use = (global.dir_exists(dev_path)) and dev_path or upstream_path
+   
 
 ---@module 'blink.cmp'
 ---@type blink.cmp.Config
@@ -45,12 +42,11 @@ require('blink.cmp').setup({
   },
 
   snippets = { preset = 'default' },
-  -- snippets = { preset = 'luasnip' },
   sources = {
     providers = {
       snippets = {
         opts = {
-          search_paths = { typst_path }
+          search_paths = { path_to_use }
         },
       },
     },
